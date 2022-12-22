e8bd60be4cc3cf05921c90966e91f0019209ffd9
jake <jakebrown536@gmail.com>
2022-12-21 22:32:48 -0700

    rocket

---
+++ src/RocketLauncher.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react'
+
+const RocketLauncher = (props) => {
+  let counter = 300000
+
+  useEffect(() => {
+    const countdown = () => console.log(counter -= 1)
+    console.log('Rocket launcher mounted')
+    let interval = setInterval(countdown, 1000)
+
+    return() => {
+        console.log('rocket launch unmounted')
+        clearInterval(interval)
+    }
+    
+  })
+
+  return (
+    <p>Rocket launcher is counting down</p>
+  )
+}
+
+export default RocketLauncher
